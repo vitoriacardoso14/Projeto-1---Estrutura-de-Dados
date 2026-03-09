f28e40c0b24bc5504ddc1c3d3e4872a1562204ac
Vitória Cardoso Alves <163563133+vitoriacardoso14@users.noreply.github.com>
2026-03-09 13:17:43 +0000

    09/03

---
+++ main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner ent = new Scanner(System.in);
+
+        println("Sistema de atendimento de Emergência Urbana")
+        println("1. Cadastrar chamado");
+        println("2. Realizar atendimento chamado");
+        println("3. Concluir atendimento");
+        println("4. Mostrar chamados abertos");
+        println("5. Mostrar lista ativa de atendimentos");
+        println("6. Mostrar histórico completo dos chamados");
+        println("7. Mostrar estatística dos níveis de emergência");
+        println("8. Mostrar os chamados com nível de emergência");
+        println("9. Mostrar Ranking de bairros");
+        println("10. Simular Cadastro de Chamado");
+        println("11. Sair");
+        println("Opção: ");
+
+        String opcao = ent.nextLine();
+
+        switch(opcao) {
+            case "1":
+        }
+
+        ent.close();
+    }
+}
